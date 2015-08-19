0d1251dc29db558bad9a315e6a3e905828cd3e2a
Cristhian Motoche <cmotoche@stackbuilders.com>
2015-08-19 12:11:55 -0500

    Libraries added to take the port

--- src/Main.hs
+++ src/Main.hs
@@ -2,20 +2,24 @@
 import Web.Scotty
 import Data.Person
 import Data.Text.Lazy (pack)
+import System.Environment
+import Control.Monad
 
 main :: IO ()
-main = scotty 3000 $ do
+main = do
+  port <- liftM read $ getEnv  "PORT"
+  scotty port $ do
 
-  let c = Person "21345697" "Camm" 21
-  get "/" $ do
-    json c
+    let c = Person "21345697" "Camm" 21
+    get "/" $ do
+      json c
 
-  delete "/" $ do
-    html "deleted!"
+    delete "/" $ do
+      html "deleted!"
 
-  post "/" $ do
-    myPerson <- jsonData
-    text $ pack $ name myPerson
+    post "/" $ do
+      myPerson <- jsonData
+      text $ pack $ name myPerson
 
-  put "/" $ do
-    html "put-ted!"
+    put "/" $ do
+      html "put-ted!"
